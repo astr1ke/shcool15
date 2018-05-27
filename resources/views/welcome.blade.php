aef4c1d7c596292e157ca005d890a1521e16a270
astrike <astrike_pro@users.noreply.github.com>
2018-05-27 03:06:51 +0300

    e=v0.9.2

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -138,6 +138,25 @@
     <!--/.container-->
 </section>
 <!--/#partner-->
+    <section id="conatcat-info">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-8">
+                    <div class="media contact-info wow fadeInDown" data-wow-duration="1000ms" data-wow-delay="600ms">
+                        <div class="pull-left">
+                            <i class="fa fa-phone"></i>
+                        </div>
+                        <div class="media-body">
+                            <h2>У вас есть к нам вопросы?</h2>
+                            <p>Можете отправить нам сообщение через форму отправки сообщений на странице контакты или позвонить по телефону +7 (86140) 55555. </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!--/.container-->
+    </section>
+    <!--/#conatcat-info-->
 
     <div class="map" >
 
@@ -148,6 +167,8 @@
 
 
     </div>
+
+
 
 
 @stop
